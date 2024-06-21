dd2f3c81279ae6c7d9d976225f70d6f972e3e6db
aidanCQ <aidan.keay@quantinuum.com>
2024-06-21 18:17:46 +0100

    extend template context.

--- quantinuum_docs_theme/__init__.py
+++ quantinuum_docs_theme/__init__.py
@@ -3,8 +3,12 @@
 from pathlib import Path
 from typing import Dict
 from sphinx.application import Sphinx
-
+from sphinx import version_info as sphinx_version
 __version__ = "0.0.1"
+
+def extend_html_context(app, pagename, templatename, context, doctree):
+     # Add ``sphinx_version_info`` tuple for use in Jinja templates
+     context['sphinx_version_info'] = sphinx_version
 
 def setup(app: Sphinx) -> Dict[str, str]:
     """Setup the Sphinx application."""
@@ -15,4 +19,8 @@ def setup(app: Sphinx) -> Dict[str, str]:
     # Include component templates
     # app.config.templates_path.append(str(theme_path / "components"))
 
+
+    # Extend the default context when rendering the templates.
+    app.connect("html-page-context", extend_html_context)
+    
     return {"parallel_read_safe": True, "parallel_write_safe": True}
