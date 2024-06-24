75e2b78259d205ad96342c5a40f7af1a2538b43d
Aidan <104831665+aidanCQ@users.noreply.github.com>
2024-06-24 14:54:54 +0100

    Add publish workflow (#1)
    * New test branch
    
    * Try theme option.
    
    * Remove default.
    
    * Use theme_
    
    * extend template context.
    
    * extend sphinx book theme layout
    
    * Does it need exclaim?
    
    * Load theme options via context because it ain't playin ball
    
    * Pass through theme options;
    
    * Will probably break
    
    * Parse as json.
    
    * Fix options.
    
    * Don't use exclaim.
    
    * Change inheritance
    
    * Attempt css import.
    
    * Dyanmic ui build.
    
    * Update readme.
    
    * Add publish workflow.
    
    * Install deps.
    
    ---------
    
    Co-authored-by: aidanCQ <aidan.keay@quantinuum.com>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Quantinuum Docs Theme
+Quantinuum Docs Sphinx Theme
+
+# /quantinuum_docs_theme 
+Contains the theme definition and assets.
+
+
+
+# Build the sphinx theme
+`./build.sh # Outputs sphinx theme files in /dist.`
---
+++ quantinuum_docs_theme/__init__.py
@@ -0,0 +1,29 @@
+"""Bootstrap-based sphinx theme from the community."""
+
+from pathlib import Path
+from typing import Dict
+from sphinx.application import Sphinx
+from sphinx import version_info as sphinx_version
+from pydata_sphinx_theme.utils import get_theme_options_dict
+__version__ = "0.0.1"
+
+def extend_html_context(app, pagename, templatename, context, doctree):
+     # Add ``sphinx_version_info`` tuple for use in Jinja templates
+     context['sphinx_version_info'] = sphinx_version
+     context['theme_options'] = get_theme_options_dict(app);
+     print(context['theme_options'])
+
+def setup(app: Sphinx) -> Dict[str, str]:
+    """Setup the Sphinx application."""
+    here = Path(__file__).parent.resolve()
+    theme_path = here / "theme" / "quantinuum_docs_theme"
+
+    app.add_html_theme("quantinuum_docs_theme", str(theme_path))
+    # Include component templates
+    # app.config.templates_path.append(str(theme_path / "components"))
+
+
+    # Extend the default context when rendering the templates.
+    app.connect("html-page-context", extend_html_context)
+    
+    return {"parallel_read_safe": True, "parallel_write_safe": True}
