f4223ef0c4199f444eede9170dcb5231efa3008b
aidanCQ <aidan.keay@quantinuum.com>
2024-06-24 18:18:38 +0100

    Use different context access.

--- quantinuum_docs_theme/__init__.py
+++ quantinuum_docs_theme/__init__.py
@@ -3,15 +3,9 @@
 from pathlib import Path
 from typing import Dict
 from sphinx.application import Sphinx
-from sphinx import version_info as sphinx_version
-from pydata_sphinx_theme.utils import get_theme_options_dict
+
 __version__ = "0.0.1"
 
-def extend_html_context(app, pagename, templatename, context, doctree):
-     # Add ``sphinx_version_info`` tuple for use in Jinja templates
-     context['sphinx_version_info'] = sphinx_version
-     context['theme_options'] = get_theme_options_dict(app);
-     print(context['theme_options'])
 
 def setup(app: Sphinx) -> Dict[str, str]:
     """Setup the Sphinx application."""
@@ -22,8 +16,4 @@ def setup(app: Sphinx) -> Dict[str, str]:
     # Include component templates
     # app.config.templates_path.append(str(theme_path / "components"))
 
-
-    # Extend the default context when rendering the templates.
-    app.connect("html-page-context", extend_html_context)
-    
     return {"parallel_read_safe": True, "parallel_write_safe": True}
