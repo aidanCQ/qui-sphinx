937cd62cda3262f1884539e7c8609ed2b5d102ec
aidanCQ <aidan.keay@quantinuum.com>
2024-06-24 15:00:29 +0100

    Copy over necessary python files to dist.

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ npm run build
 cp ./node_modules/@cqcl/quantinuum-ui/dist/tokens.css ./static/tokens.css
 npx tailwindcss --postcss ./postcss.config.cjs -i ./ui/index.css -o ./static/styles.css
 cp -R ./static $STATIC_DIR
+cp ./poetry.lock ./dist/poetry.lock
+cp ./pyproject.toml ./dist/pyproject.toml
+cp ./README.md ./dist/README.md
 rm -rf ./static
 echo ✅ "Done. Generated sphinx theme under /dist."
 
